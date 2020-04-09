d1d9d3a4a81ae9b2ca48f32d852ec9d15370d72e
rozay10 <raadeniyi3@gmail.com>
2020-04-09 08:01:55 +0100

    finished challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+// import data from './data';
+
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases } = data;
+
+  const keyImpact = () => reportedCases * 10;
+  const keySevereImpact = () => reportedCases * 50;
+  const infectionsByRequestedTime = (currentlyInfected, days = 30) => {
+    const factor = Math.floor(days / 3);
+    return currentlyInfected * factor ** 2;
+  };
+  return {
+    data,
+    impact: {
+      currentlyInfected: keyImpact(),
+      infectionsByRequestedTime: infectionsByRequestedTime(keyImpact)
+    },
+    severeImpact: {
+      currentlyInfected: keySevereImpact(),
+      infectionsByRequestedTime: infectionsByRequestedTime(keySevereImpact)
+    }
+  };
+};
+
+
+// covid19ImpactEstimator(data);
 
 export default covid19ImpactEstimator;
